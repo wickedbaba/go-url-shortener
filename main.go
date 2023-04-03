0244c231b8b6915a4851194b45c5ee101399b840
wickedbaba <aryamansinghranaasr@gmail.com>
2023-04-03 17:41:08 +0530

    Worked on initialisation

--- main.go
+++ main.go
@@ -2,12 +2,36 @@ package main
 
 import (
 	"fmt"
+	"log"
+
+	"github.com/gin-gonic/gin"
 )
 
 var pl = fmt.Println
 var pf = fmt.Printf
 
+func errChecker(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
 func main() {
 	pl("the code is working")
 	pf("\nfine\n")
+
+	// Default returns an Engine instance with the Logger
+	// and Recovery middleware already attached.
+	r := gin.Default()
+
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "Hey Go URL Shortener!",
+		})
+	})
+
+	err := r.Run(":8080")
+
+	errChecker(err)
+
 }
